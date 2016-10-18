8d3c789170116489f9547bf57f6727b5ead21b68
Joel Raj <Joel Raj>
2016-10-18 18:51:23 -0400

    Added most of the objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,64 @@
-$("#main").append("Joel Rajkumar");
+var bio = {
+	name : "Joel Rajkumar",
+	age : "23",
+	role : "Web Developer",
+	contact : {
+		email : "JoelRajkumar@yahoo.com",
+		mobile : "646-221-5421",
+		github : "joelkraj",
+		twitter : "joelkraj",
+		location : "New York"
+	},
+	welcomeMessage : "Hello there! lorem ipsum dolor sit",
+	skills : ["programming", "JS", "HTML", "CSS"],
+	bioPic : "images/fry.jpg"
+};
 
-var awesomeThoughts = "I am Joel and I am AWESOME!"
+var education = {
+	Schools : {
+		name : "Stony Brook University",
+		city : "Stony Brook, NY",
+		degree : "Bachelors(inprogress)",
+		majors : "CS",
+		dates : "2015, 2017, 2018",
+		url : "www.stonybrook.edu"
+	},
+	onlineCourses : [
+		{
+			title : "Frontend Nanodegree",
+			school : "Udacity",
+			dates : "2016",
+			url	: "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+		},
+		{
+			title : "Web Developer Bootcamp",
+			school : "Udemy",
+			dates : "2016",
+			url	: "https://www.udemy.com/the-web-developer-bootcamp/learn/v4/"
+		}
+	]
+}
 
-console.log(awesomeThoughts);
+var work = {
+	jobs : [
+		{
+			employer : "US Army",
+			title : "Army Specialist",
+			dates : "2013-current",
+			description : "I am a health care specialist in the Us Army reserve."
+		},
+		{
+			employer : "Rockaway Party Supply Store",
+			title : "Cashier",
+			dates : "2009-2013",
+			description : "I worked as a cashier at my family owned store. I worked with many great people and interacted with customers daily."
+		}
+	]
+}
 
-var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-
-console.log(funThoughts);
+var projects = {
+	projects : [
+	{
+		title : "Responsive Bootstrap 4"
+	}]
+}
